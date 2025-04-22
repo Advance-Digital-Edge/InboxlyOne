2e7107db25783fbbae73d5a28afa7e7beca5f951
ajummer <makeudreamempty@gmail.com>
2025-04-22 14:43:44 +0300

    feat: add components to home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,14 @@
+// app/page.tsx
+
 import Hero from "@/components/sections/Hero/Hero";
 import Features from "@/components/sections/Features/Features";
 import HowItWorks from "@/components/sections/HowItWorks/HowItWorks";
 import Pricing from "@/components/sections/Pricing/Pricing";
 import Testimonials from "@/components/sections/Testimonials/Testimonials";
 import CTA from "@/components/sections/CTA/Cta";
+import { JSX } from "react";
 
-export default async function Home() {
+export default function Home(): JSX.Element {
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
       <Hero />
