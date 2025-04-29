d242dffbe377b631e0b2dd129c743da35770ceff
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2025-04-29 14:30:52 +0300

    feat: change navbar depending on authentication

--- client/components/layout/Navbar.tsx
+++ client/components/layout/Navbar.tsx
@@ -1,11 +1,31 @@
+"use client"
+
 import Link from "next/link"
 import { Inbox } from "lucide-react"
 import { Button } from "@/components/ui/Button/button"
 import styles from "./navbar.module.css"
 import MobileMenu from "./NavbarMobile"
 import { JSX } from "react";
+import {useState, useEffect} from "react";
+import { useAuth } from "@/app/context/AuthProvider"
+import { signOutAction } from "@/app/actions"
 
 export default function Navbar(): JSX.Element {
+  const [hasUser, setHasUser] = useState<boolean | null>(false);
+  const { user, setUser } = useAuth();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    setHasUser(!!user);
+  }, [user]);
+
+  const signOutHandler = async (): Promise<void> => {
+    localStorage.removeItem("user");
+    setUser(null);
+    setHasUser(false);
+    await signOutAction();
+  };
+
   return (
     <header className={styles.header}>
       <Link href={"/"}>
@@ -29,9 +49,21 @@ export default function Navbar(): JSX.Element {
         </Link>
       </nav>
       <div className={styles.headerButtons}>
-        <Link href="/sign-in" className={styles.loginLink}>
-          Log in
-        </Link>
+      {!hasUser ? (
+          <>
+            <Link href="/sign-in" className={styles.loginLink}>
+              Log in
+            </Link>
+          </>
+        ) : (
+          <Button 
+            className={styles.loginLink} 
+            style={{backgroundColor: "red"}}
+            onClick={signOutHandler}
+            >
+            Logout
+          </Button>
+        )}
         <Button className={styles.ctaButton}>Get Started</Button>
       </div>
       <MobileMenu />
